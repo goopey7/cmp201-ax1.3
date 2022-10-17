ab0449330b7dfbe303792f2b51708e510edc2d3f
Sam Collier <sam@samcollier.tech>
2022-10-17 18:30:19 +0000

    First Submission Attempt

--- Search.cpp
+++ Search.cpp
@@ -10,6 +10,7 @@
 #include <iostream>
 #include <chrono>		// for timing
 #include <map>
+#include <cmath>
 
 
 // Return index position of key in arr or -1 if not in collection.
@@ -17,7 +18,14 @@
 int LinearScan(std::vector<int> arr, int key)
 {
 	// TODO: Assessed [1]
-	return 0;
+	for(int i=0; i<arr.size(); i++)
+	{
+		if(arr[i] == key)
+		{
+			return i;
+		}
+	}
+	return -1;
 }
 
 // Return index position of key in arr or -1 if not in collection.
@@ -25,7 +33,30 @@ int LinearScan(std::vector<int> arr, int key)
 int BinarySearch(std::vector<int> arr, int key)
 {
 	// TODO: Assessed [1]
-	return 0;
+
+	int start = 0;
+	int end = arr.size()-1;
+
+	while(start <= end)
+	{
+		int mid = std::floor((start+end)/2.f);
+
+		if(arr[mid] == key)
+		{
+			return mid;
+		}
+
+		if(key < arr[mid])
+		{
+			end = mid-1;
+		}
+		else
+		{
+			start = mid+1;
+		}
+	}
+
+	return -1;
 }
 
 // Return index position of key in arr or -1 if not in collection.
@@ -33,7 +64,34 @@ int BinarySearch(std::vector<int> arr, int key)
 int InterpolationSearch(std::vector<int> arr, int key)
 {
 	// TODO: Assessed [1]
-	return 0;
+
+	int start = 0;
+	int end = arr.size()-1;
+
+	while(start <= end && key >= arr[start] && key <= arr[end])
+	{
+		int dist = key - arr[start];
+		int valRange = arr[end]-arr[start];
+		int fraction = std::floor((float)dist/valRange);
+		int indexRange = end-start;
+		int estimate = start + fraction * indexRange;
+
+		if(arr[estimate] == key)
+		{
+			return estimate;
+		}
+
+		if(key < arr[estimate])
+		{
+			end = estimate-1;
+		}
+		else
+		{
+			start = estimate+1;
+		}
+	}
+
+	return -1;
 }
 
 /*
@@ -44,7 +102,44 @@ std::vector<int> SortedVectorGenerator(int size)
 {
 	// TODO: Unassessed.
 		// Check with implementation from partial framework before submitting.
-	return std::vector<int>();
+
+	srand(time(0));
+	std::vector<int> v;
+
+	if(size > 0)
+	{
+		v.push_back(rand() % 5 + 1);
+	}
+
+	for(int i=1;i<size;i++)
+	{
+		v.push_back(v[i-1] + rand() % 5 + 1);
+	}
+
+	return v;
+}
+
+std::vector<int> EvenlyDistributedVectorGenerator(int size)
+{
+	// TODO: Unassessed.
+		// Check with implementation from partial framework before submitting.
+
+	srand(time(0));
+	std::vector<int> v;
+
+	int dist = rand() % 5 + 1;
+
+	if(size > 0)
+	{
+		v.push_back(dist);
+	}
+
+	for(int i=1;i<size;i++)
+	{
+		v.push_back(v[i-1] + dist);
+	}
+
+	return v;
 }
 
 // Runs the three searches on the three vectors with the three targets supplied as parameters.
@@ -59,8 +154,65 @@ runTests(std::vector<int> v0, std::vector<int> v1, std::vector<int> v2,
 int t0, int t1, int t2)
 {
 	// TODO: Assessed [2]
+
+	//KEYS
+	const std::string LINEAR_SCAN = "linear scan";
+	const std::string BINARY_SEARCH = "binary search";
+	const std::string INTERPOLATION_SEARCH = "interpolation search";
+
+	std::map<std::string,std::vector<double>> results;
+
+	// linear scan
+	auto start = std::chrono::high_resolution_clock::now();
+	LinearScan(v0,t0);
+	auto end = std::chrono::high_resolution_clock::now();
+	results[LINEAR_SCAN].push_back(std::chrono::duration_cast<std::chrono::nanoseconds>(end-start).count());
 	
-	return std::map<std::string, std::vector<double>>();
+	start = std::chrono::high_resolution_clock::now();
+	LinearScan(v1,t1);
+	end = std::chrono::high_resolution_clock::now();
+	results[LINEAR_SCAN].push_back(std::chrono::duration_cast<std::chrono::nanoseconds>(end-start).count());
+
+	start = std::chrono::high_resolution_clock::now();
+	LinearScan(v2,t2);
+	end = std::chrono::high_resolution_clock::now();
+	results[LINEAR_SCAN].push_back(std::chrono::duration_cast<std::chrono::nanoseconds>(end-start).count());
+
+
+	// binary search
+	start = std::chrono::high_resolution_clock::now();
+	BinarySearch(v0,t0);
+	end = std::chrono::high_resolution_clock::now();
+	results[BINARY_SEARCH].push_back(std::chrono::duration_cast<std::chrono::nanoseconds>(end-start).count());
+
+	start = std::chrono::high_resolution_clock::now();
+	BinarySearch(v1,t1);
+	end = std::chrono::high_resolution_clock::now();
+	results[BINARY_SEARCH].push_back(std::chrono::duration_cast<std::chrono::nanoseconds>(end-start).count());
+
+	start = std::chrono::high_resolution_clock::now();
+	BinarySearch(v2,t2);
+	end = std::chrono::high_resolution_clock::now();
+	results[BINARY_SEARCH].push_back(std::chrono::duration_cast<std::chrono::nanoseconds>(end-start).count());
+
+
+	// interpolation search
+	start = std::chrono::high_resolution_clock::now();
+	InterpolationSearch(v0,t0);
+	end = std::chrono::high_resolution_clock::now();
+	results[INTERPOLATION_SEARCH].push_back(std::chrono::duration_cast<std::chrono::nanoseconds>(end-start).count());
+
+	start = std::chrono::high_resolution_clock::now();
+	InterpolationSearch(v1,t1);
+	end = std::chrono::high_resolution_clock::now();
+	results[INTERPOLATION_SEARCH].push_back(std::chrono::duration_cast<std::chrono::nanoseconds>(end-start).count());
+
+	start = std::chrono::high_resolution_clock::now();
+	InterpolationSearch(v2,t2);
+	end = std::chrono::high_resolution_clock::now();
+	results[INTERPOLATION_SEARCH].push_back(std::chrono::duration_cast<std::chrono::nanoseconds>(end-start).count());
+
+	return results;
 	
 }
 
@@ -86,7 +238,7 @@ int main()
 	// Prepare vectors and targets for test.
 	std::vector<int> v0 = SortedVectorGenerator(1000);
 	std::vector<int> v1 = SortedVectorGenerator(10000);
-	std::vector<int> v2 = SortedVectorGenerator(100000);
+	std::vector<int> v2 = EvenlyDistributedVectorGenerator(100000);
 	int t0 = v0[10];			
 	int t1 = v1[4999];
 	int t2 = 1000000*5 + 1;
@@ -105,6 +257,4 @@ int main()
 		std::cout << p.second[0] << ", " << p.second[1] << ", " << p.second[2] << std::endl;
 	}
 }
-
-
 
